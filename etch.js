f947e334b1e0adc35f4cd4a10ab99caca3f16174
Michael Smith <digitalmikemsith@gmail.com>
2020-05-21 15:32:37 +0100

    initalised grid

---
+++ etch.js
@@ -0,0 +1,9 @@
+const game = document.querySelector('#root')
+
+function create_square_grid(lenOFGrid){
+
+   for (let i=0;i=lenOFGrid;i++){
+       console.log(i)
+   return game.createElement('div[class = square]')} //* (lenOFGrid)**2
+    //document.createElement(div)
+}
